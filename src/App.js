8643ad2df3d9851a32a4f910aea1bd6967c602fc
narukags <naruka.gajraj07@gmail.com>
2021-03-23 19:17:47 +0530

    This is shadow box generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
 function App() {
+  const [Hori, setHori] = useState(10);
+  const [Veri, setVeri] = useState(10);
+  const [Blur, setBlur] = useState(10);
+  const [color, setColor] = useState("black");
+  const [checkOn, setCheckOn] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="controls">
+        <lable >Horizontal length</lable>
+        <input type="range" min="-200" max="200" value={Hori} onChange={(e) => setHori(e.target.value)} />
+        <lable >Verticle length</lable>
+        <input type="range" min="-200" max="200" value={Veri} onChange={(e) => setVeri(e.target.value)} />
+        <lable >blur</lable>
+        <input type="range" min="0" max="200" value={Blur} onChange={(e) => setBlur(e.target.value)} />
+        <lable >color</lable>
+        <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
+        <div className="switch">
+          <label>
+            Outline
+          <input type="checkbox" checked={checkOn} onChange={() => setCheckOn(!checkOn)} />
+            <span className="lever"></span>
+          insite
+        </label>
+        </div>
+
+      </div>
+      <div className="output"></div>
+      <div className="box" style={{ boxShadow: `${checkOn ? "inset" : ""} ${Hori}px ${Veri}px ${Blur}px ${color}` }}>
+        <p>box-shadow:{Hori}px {Veri}px {Blur}px {color}</p>
+
+      </div>
+
+
     </div>
   );
 }
